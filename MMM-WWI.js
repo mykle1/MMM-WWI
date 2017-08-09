a82888a6e9e40a356a2fd6488cee55069f735880
Mykle <themykle@imykle.com>
2017-08-08 22:48:02 -0400

    Add files via upload

---
+++ MMM-WWI.js
@@ -0,0 +1,230 @@
+/* Magic Mirror
+ * Module: MMM-WWI
+ * Weather Without Icons
+ * By Mykle1 - [Powered by Dark Sky](https://darksky.net/poweredby/)
+ * MIT Licensed
+ */
+Module.register("MMM-WWI", {
+
+    // Module config defaults.
+    defaults: {
+		apiKey: "",                               // Get FREE API key from https://darksky.net
+        lat: "",                                  // latitude
+        lng: "",                                  // longitude
+		pix: "1",                                 // 1-13
+        useHeader: false,                         // true if you want a header      
+        header: "Weather Without Icons",          // Any text you want. useHeader must be true
+        maxWidth: "300px",
+        animationSpeed: 3000,
+        initialLoadDelay: 4250,
+        retryDelay: 2500,
+        updateInterval: 2 * 60 * 1000,           // 2 minutes is the least you can use for free
+
+    },
+
+    getStyles: function() {
+        return ["MMM-WWI.css"];
+    },
+
+    getScripts: function() {
+        return ["moment.js"];
+    },
+
+    start: function() {
+        Log.info("Starting module: " + this.name);
+
+        requiresVersion: "2.1.0",
+
+        //  Set locale.
+        this.url = "https://api.darksky.net/forecast/" + this.config.apiKey + "/" + this.config.lat + "," + this.config.lng;
+        this.WWI = {};
+        this.scheduleUpdate();
+    },
+
+    getDom: function() {
+
+        var wrapper = document.createElement("div");
+        wrapper.className = "wrapper";
+        wrapper.style.maxWidth = this.config.maxWidth;
+
+        if (!this.loaded) {
+            wrapper.innerHTML = "Not that WWI!";
+            wrapper.classList.add("bright", "light", "small");
+            return wrapper;
+        }
+
+        if (this.config.useHeader != false) {
+            var header = document.createElement("header");
+            header.classList.add("xsmall", "bright", "light");
+            header.innerHTML = this.config.header;
+            wrapper.appendChild(header);
+        }
+
+        var WWI = this.WWI;
+		var apiKey = this.config.apiKey;
+        var lat = this.config.lat; // latitude
+        var lng = this.config.lng; // longitude
+		var pix = this.config.pix; // 1-13
+
+
+        var top = document.createElement("div");
+        top.classList.add("list-row");
+		
+		
+		
+////////// alerts sre not always in the data /////////////////////////////////
+////////// but I want them when they do. How? //////////////////////////////
+    //    Alert object does not always appear in data ////////////////////////////////////
+		// How do I do it?
+		// type of warning
+    //    var type = document.createElement("div");
+    //    type.classList.add("xsmall", "bright", "type");
+		
+////////// this isn't working. console reports title is undefined
+	//	if (WWI.alerts[0].title != undefined){
+	//		type.innerHTML = WWI.alerts[0].title + "!";
+	//	}
+	//	wrapper.appendChild(type);
+	
+	
+		// summary of weather at this moment
+        var summary = document.createElement("div");
+        summary.classList.add("small", "bright", "summary");
+        summary.innerHTML = WWI.currently.summary + " and " + Math.round(WWI.currently.temperature) + "° F at " + moment(WWI.time).local().format("h:mm A");
+        wrapper.appendChild(summary);
+		
+		
+		// summary prediction of weather for coming week
+        var summary = document.createElement("div");
+        summary.classList.add("small", "bright", "summary");
+        summary.innerHTML = WWI.minutely.summary; // can I put icon here too?
+        wrapper.appendChild(summary);
+		
+	
+		// can I rotate the pictures during update interval?
+	    // Choose a picture in config 1-13
+        var pic = document.createElement("div");
+        var img = document.createElement("img");
+        img.classList.add("photo");
+	if (this.config.pix != ""){
+		img.src = "modules/MMM-WWI/pix/" + this.config.pix + ".jpg";
+		}
+        pic.appendChild(img);
+        wrapper.appendChild(pic);
+		
+		
+		// weekly summary
+        var summary = document.createElement("div");
+        summary.classList.add("xsmall", "bright", "summary");
+        summary.innerHTML = WWI.daily.summary;
+        wrapper.appendChild(summary);
+		
+		
+		// heat index/wind chill
+        var temperature = document.createElement("div");
+        temperature.classList.add("xsmall", "bright", "chill");
+        temperature.innerHTML = "Heat index/Wind chill = " + Math.round(WWI.currently.apparentTemperature) + "° F";
+        wrapper.appendChild(temperature);
+		
+		
+		// ozone at this moment
+        var ozone = document.createElement("div");
+        ozone.classList.add("xsmall", "bright", "ozone");
+        ozone.innerHTML = "Ground-Level Ozone is " + Math.round(WWI.currently.ozone);
+        wrapper.appendChild(ozone);
+		
+		
+		// distance of storm weather at this moment
+        var distance = document.createElement("div");
+        distance.classList.add("xsmall", "bright", "StormDistance");
+        distance.innerHTML = "A storm is " + WWI.currently.nearestStormDistance + " miles away!"; // can I put icon here too?
+        wrapper.appendChild(distance);
+		
+		
+		// nearestStormBearing at this moment
+        var nearestStormBearing = document.createElement("div");
+        nearestStormBearing.classList.add("xsmall", "bright", "StormBearing");
+        nearestStormBearing.innerHTML = "Storm bearing is " + WWI.currently.nearestStormBearing + "°";
+        wrapper.appendChild(nearestStormBearing);
+		
+		
+		// windSpeed at this moment
+        var windSpeed = document.createElement("div");
+        windSpeed.classList.add("xsmall", "bright", "windSpeed");
+        windSpeed.innerHTML = "Wind speed is " + WWI.currently.windSpeed + " mph";
+        wrapper.appendChild(windSpeed);
+		
+		
+		// windGust at this moment
+        var windGust = document.createElement("div");
+        windGust.classList.add("xsmall", "bright", "windGust");
+        windGust.innerHTML = "Wind gusts to " + WWI.currently.windGust + " mph";
+        wrapper.appendChild(windGust);
+		
+		
+		
+		// windBearing at this moment
+        var windBearing = document.createElement("div");
+        windBearing.classList.add("xsmall", "bright", "windBearing");
+        windBearing.innerHTML = "Wind bearing is " + WWI.currently.windBearing + "°";
+        wrapper.appendChild(windBearing);
+		
+		
+		// visibility at this moment
+        var visibility = document.createElement("div");
+        visibility.classList.add("xsmall", "bright", "visibility");
+        visibility.innerHTML = "Visibility is " + WWI.currently.visibility + " miles";
+        wrapper.appendChild(visibility);
+		
+		
+		// pressure at this moment
+        var pressure = document.createElement("div");
+        pressure.classList.add("xsmall", "bright", "pressure");
+        pressure.innerHTML = "Barometer = " + Math.round(WWI.currently.pressure) + " mb";
+        wrapper.appendChild(pressure);
+		
+		
+		// dewPoint at this moment
+        var dewPoint = document.createElement("div");
+        dewPoint.classList.add("xsmall", "bright", "dewPoint");
+        dewPoint.innerHTML = "Dew point is " + WWI.currently.dewPoint;
+        wrapper.appendChild(dewPoint);
+		
+		
+		// uvIndex at this moment
+        var uvIndex = document.createElement("div");
+        uvIndex.classList.add("xsmall", "bright", "uvIndex");
+        uvIndex.innerHTML = "UV Index is " + WWI.currently.uvIndex;
+        wrapper.appendChild(uvIndex);
+	
+	
+        return wrapper;
+    },
+
+
+    processWWI: function(data) {
+        this.today = data.Today;
+        this.WWI = data;
+        this.loaded = true;
+    },
+
+    scheduleUpdate: function() {
+        setInterval(() => {
+            this.getWWI();
+        }, this.config.updateInterval);
+        this.getWWI(this.config.initialLoadDelay);
+    },
+
+    getWWI: function() {
+        this.sendSocketNotification('GET_WWI', this.url);
+    },
+
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === "WWI_RESULT") {
+            this.processWWI(payload);
+
+            this.updateDom(this.config.animationSpeed);
+        }
+        this.updateDom(this.config.initialLoadDelay);
+    },
+});
--- README.md
+++ README.md
@@ -1,2 +1,73 @@
-# MMM-WWI
-Weather Without Icons
+## MMM-WWI
+
+**Weather Without Icons**
+
+## But Mykle, why another weather module?
+
+I enjoy doing this. I'm not trying to compete with the *real* geniuses here.
+They are truly amazing and worthy of praise. I'm just having fun.
+I'm still surprised that I can make *any* module work.
+That's mostly due to the patience and generosity of my friend at the forum.
+He is one of those genius types that I am talking about. :^)
+
+## The information 
+
+* Current conditions, temperature and time
+* Conditions for the next hour
+* A summary for the next 7 days
+* Pictures of lightning events (1-13) if you want them
+* Heat index/Wind chill temperature at this moment
+* Nearest storm/weather event at this moment
+* Bearing of the nearest storm/weather event at this moment
+* Wind speed for your location at this moment
+* Wind gusts speed for your location at this moment
+* Wind bearing at this moment
+* Visibility (in miles) at this moment
+* Barometer reading at this moment (in millibars)
+* DewPoint reading at this moment
+* UV Index reading at this moment
+
+## Examples
+
+![](pix/WWI.JPG)
+
+Annotated .css file included for aligning and coloring text and header.
+
+## Installation and requirements
+
+* `git clone https://github.com/mykle1/MMM-WWI` into the `~/MagicMirror/modules` directory.
+
+* Free API key at `https://darksky.net/dev/register` using just your email.
+
+* No dependencies needed! No kidding!
+
+
+## Config.js entry and options
+
+    {
+		disabled: false,
+		module: "MMM-WWI",
+		position: "top_left",
+		config: {
+			apiKey: "YOUR API KEY GOES HERE", // Free API key @ https://darksky.net
+			lat: "YOUR LATITUDE GOES HERE",   // Your latitude goes here
+			lng: "YOUR LONGITUDE GOES HERE",  // Your longitude goes here
+			pix: "4",                         // 1-13
+			useHeader: false,                 // true if you want a header                 
+			header: "Weather Without Icons",
+			maxWidth: "300px",
+		}
+	},
+	
+## If you want icons
+
+```
+		var messageToYou = document.createElement("div");
+		messageToYou.classList.add("U.R.xsmall", "notTooBright", "uglyToo");
+	if(you.want.icons && a.module.by.a.genius){
+		messageToYou.innerHTML = "Install MMM-NOAA instead";
+	}
+		wrapper.appendChild(messageToYou);
+```
+
+## [Powered by Dark Sky](https://darksky.net/poweredby/)
